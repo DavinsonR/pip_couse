e448398aed34b4d3b74026f05594ad2d5268319e
CoderDavinson <Davinsonnovoaramirez@gmail.com>
2023-05-28 23:03:51 -0500

    using fastAPI to deply a prove page with response

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,30 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return [1,2,4,5]
+
+@app.get('/contact')
+def get_json():
+    return {'name': 'Payout-them'}
+
+@app.get('/items/', response_class = HTMLResponse)
+async def read_items():
+    return """
+    <html>
+        <head>this is the title in page
+        </head>
+            <body>
+            <h1> this is the title inside<h1/>
+            <p>lorem impsum, look at this mom</p>
+            </body>
+    </html>
+    """
+
 
 def run():
     store.get_categories()
